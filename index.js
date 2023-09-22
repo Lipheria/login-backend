72e02cd6986ff18f780d4e8a96ae769faf7c7d71
Lipheria <64316193+Lipheria@users.noreply.github.com>
2023-09-22 06:01:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const path = require('path')
 
 app.use(express.json())
 app.use(cors({
-  origin: ["https://login-register-dashboard.vercel.app/"],
+  origin: ["https://login-register-dashboard.vercel.app"],
   methods: ["GET","HEAD","PUT","PATCH","POST","DELETE"],
   credentials: true,
   preflightContinue: false
